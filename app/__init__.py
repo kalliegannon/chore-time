58363424715bd3b101fa96eb99b368823290a792
Kallie Gannon <gannonkallie@gmail.com>
2023-01-30 13:42:00 -0500

    innit imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
 
 
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
